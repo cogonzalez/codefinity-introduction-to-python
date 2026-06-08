e57848daebb6c1463a63ed6f49bdca9cf23f755c
cogonzalez <>
2026-06-08 16:38:08 +0000

    Dictionaries

--- other_data_types/dictionaries/main.py
+++ other_data_types/dictionaries/main.py
@@ -4,10 +4,13 @@ inventory = {
     "Oranges": 18,
     "Bananas": 45
 }
-# get(): retrieves the value for a specified key, and if the key is not found, 
+
+print("Original Inventory:", inventory)
+
+# get(): retrieves the value for a specified key, 
+# and if the key is not found, 
 # it returns None. This is different from using square brackets 
 # (e.g., grocery_items["Milk"]), which would raise an error if the key doesn't exist.
-
 # Get the count of Oranges
 print("Count of Oranges:", inventory.get("Oranges"))
 
@@ -15,11 +18,14 @@ print("Count of Oranges:", inventory.get("Oranges"))
 inventory.update({"Mangoes": 20})
 print("Updated Inventory:", inventory)
 
+
 # You can also add a new item to the end of the dictionary like this
 inventory["Pineapples"] = 15
 print("Updated Inventory:", inventory)
 
+
 # Remove Bananas from the inventory
 removed_item = inventory.pop("Bananas")
 print("Removed Item:", removed_item)
+
 print("Current Inventory:", inventory)
