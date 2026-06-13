7f386d991665b75f09575dcf2490f9ccbdcb1cdb
cogonzalez <>
2026-06-13 19:26:45 +0000

    range function

--- loops/range_function/main.py
+++ loops/range_function/main.py
@@ -4,10 +4,13 @@ daily_promotions = ["Milk", "Eggs", "Bread", "Apples", "Oranges"]
 # List of weekdays corresponding to the promotions
 weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 
-# for day arbritrary variable in range(0,7,1)
-for day in range(7)
+# for day arbritrary variable in range(0,5,1) 
+# there are 5 elements in these list at positions 0, 1, 2, 3, 4
+# remember five days in list chose 5
+for day in range(5):
     # Access the corresponding weekday
     weekday = weekdays[day]
-    # Access teh corresponding daily promotion
+    # Access the corresponding daily promotion
     promotion = daily_promotions[day]
     print(f"{weekday}: Promotion on {promotion}")
+    
